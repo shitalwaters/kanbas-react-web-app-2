78742cc5b895f393e4ef49b3cb56478017f2c013
shetalw <sdwaters8@gmail.com>
2023-11-22 21:16:13 -0500

    3.2 Dashboard Screen done

---
+++ src/Kanbas/Dashboard/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Database from '../Database';
+import blueSolid from '../../blueSolid.jpg'
+
+function Dashboard() {
+  return (
+    <div className="col-md-10">
+      <h2>Published Courses</h2>
+      <div className="d-flex flex-row flex-wrap">
+        {Database.courses.map(course => (
+          <div key={course.id} className="m-3" style={{ flex: '1 0 calc(25% - 24px)', maxWidth: 'calc(25% - 24px)' }}>
+            <div className="card">
+              <img className="card-img-top" src={blueSolid} alt="Course Image" />
+              <div className="card-body">
+                <h5 className="card-title"><a href="">{course.name}</a></h5>
+                <p className="card-text">{course.startDate} to {course.endDate}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,20 +1,37 @@
 import KanbasNavigation from "./KanbasNavigation";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import "./index.css"
 
+{/*BreadCrum at the top w/ dynamically changing Title */}
+function getPageTitle(url) {
+    const pathSegments = url.split("/").filter(segment => segment.length > 0);
+    const lastSegment = pathSegments[pathSegments.length - 1];
+    return lastSegment.charAt(0).toUpperCase() + lastSegment.slice(1);
+}
 
 function Kanbas() {
+    const location = useLocation();
+    const pageTitle = getPageTitle(location.pathname);
+
     return (
         <div className="d-flex">
             <KanbasNavigation />
-        <div>
-        <Routes>
-            <Route path="/" element={<Navigate to="Dashboard" />} />
-            <Route path="Account" element={<h1>Account</h1>} />
-            <Route path="Courses/*" element={<h1>Courses</h1>} />
-            <Route path="Calendar/*" element={<h1>Calendar</h1>} />
-</Routes>
+            <div>
+                {/* Dynamic Title */}
+                <div className="page-title">
+                    <h2>{pageTitle}</h2>
+                </div>
+
+                <Routes>
+                    <Route path="/" element={<Navigate to="Dashboard" />} />
+                    <Route path="Account" element={<h1>Account</h1>} />
+                    <Route path="Dashboard" element={<Dashboard />} />
+                    <Route path="Courses/*" element={<h1>Courses</h1>} />
+                </Routes>
+            </div>
         </div>
-    </div>
     );
 }
+
 export default Kanbas;
