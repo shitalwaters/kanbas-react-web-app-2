85f1920d66c7a1f3c055b3ac281985d367220ffd
shetalw <sdwaters8@gmail.com>
2023-11-28 23:21:08 -0500

    Section 3.1, 3.1.1, 3.1.2 and 3.1.3 redone and completed

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -1,36 +1,100 @@
-import React from 'react';
+import { React, useState } from "react";
 import Database from '../Database';
-import blueSolid from '../../blueSolid.jpg'
+import blueSolid from '../../blueSolid.jpg';
 import { Link } from 'react-router-dom';
-import "./index.css"
-
+import "./index.css";
 
 function Dashboard() {
-  return (
-    <div className="col-md-10">
-        <h1>Dashboard</h1>
-        <div className="page-header"></div>
-        
-      <h2>Published Courses</h2>
-      <div className="d-flex flex-row flex-wrap">
-        {Database.courses.map(course => (
-          <div key={course._id} className="m-3" style={{ flex: '1 0 calc(25% - 24px)', maxWidth: 'calc(25% - 24px)' }}>
-           
-            <div className="card">
-              <img className="card-img-top" src={blueSolid} alt="Course Image" />
-              <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="list-group-item">
-                {course.name}
-                <p className="card-text">{course._id} </p>
-                <p className="card-text">{course.startDate} to {course.endDate}</p>
-                </Link>
+    const [courses, setCourses] = useState(Database.courses);
+    const [course, setCourse] = useState({
+      name: "New Course", number: "New Number",
+      startDate: "2023-09-10", endDate: "2023-12-15",
+      });
+      const updateCourse = () => {
+        setCourses(
+        courses.map((c) => {
+        if (c._id === course._id) {
+        return course;
+        } else {
+        return c;
+        }
+        })
+        );
+        };
+
+      const addNewCourse = () => {
+      setCourses([...courses,
+      { ...course,
+      _id: new Date().getTime() }]);
+      };
+
+      const deleteCourse = (courseId) => {
+        setCourses(courses.filter((course) => course._id !== courseId));
+        };
+    return (
+        <div className="main-container"> {/* Main container */}
+            
+            <div className="content-area col-md-8"> {/* Main content area */}
+                <h1>Dashboard</h1>
                 
-              </div>
+                <div className="page-header"></div>
+                
+                <h2>Published Courses</h2>
+
+                
+                <div className="d-flex flex-row flex-wrap">
+                    {courses.map(course => (
+                        <div key={course._id} className="m-3" style={{ flex: '1 0 calc(25% - 24px)', maxWidth: 'calc(25% - 24px)' }}>
+                            <div className="card">
+                                <img className="card-img-top" src={blueSolid} alt="Course Image" />
+                                <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="list-group-item">
+                                    {course.name}
+                                    <p className="card-text">{course._id} </p>
+                                    <p className="card-text">{course.startDate} to {course.endDate}</p>
+
+                                    <button
+                                      onClick={(event) => {event.preventDefault();setCourse(course);
+                                      }}>
+                                      Edit
+                                      </button>
+
+                                    <button
+                                      onClick={(event) => {event.preventDefault();deleteCourse(course._id);
+                                      }}>
+                                      Delete
+                                    </button>
+
+                                </Link>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-       
-        ))}
-      </div>
-    </div>
-  );
+
+            
+{/* Sidebar */}
+            <div className="sidebar col-md-4"> 
+            <button onClick={addNewCourse} > Add </button>
+            <button onClick={updateCourse} >
+Update
+</button>
+                
+              <h5>Course</h5>
+              <input value={course.name} className="form-control"
+                onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+
+                <input value={course.number} className="form-control"
+                onChange={(e) => setCourse({ ...course, number: e.target.value }) } />
+
+                <input value={course.startDate} className="form-control" type="date"
+                onChange={(e) => setCourse({ ...course, startDate: e.target.value }) }/>
+
+                <input value={course.endDate} className="form-control" type="date"
+                onChange={(e) => setCourse({ ...course, endDate: e.target.value }) } />
+            </div>
+
+        </div>
+    );
 }
 
 export default Dashboard;
