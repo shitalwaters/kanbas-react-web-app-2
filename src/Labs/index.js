086d5fabcee58bfad21a2b5a7f1db013e38b8394
shetalw <sdwaters8@gmail.com>
2023-11-28 21:20:41 -0500

    redid and completed A4 labs

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,12 +1,22 @@
+import Nav from "../nav"
 import Assignment3 from "./a3";
-import Nav from "../nav.js";
+import Assignment4 from "./a4";
+import {Routes, Route, Navigate} from "react-router";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function Labs() {
-    return(
-        <div>
-            <Nav/>
-            <Assignment3/>            
-        </div>
+    return (
+        <Provider store={store}>
+            <div>
+                <Nav/>
+                    <Routes>
+                        <Route path="/"element={<Navigate to="a3"/>}/>
+                        <Route path="a3"element={<Assignment3/>}/>
+                        <Route path="a4"element={<Assignment4/>}/>
+                    </Routes>
+            </div>
+        </Provider>
     );
 }
 export default Labs;
